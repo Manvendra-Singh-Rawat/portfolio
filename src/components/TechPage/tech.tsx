73d498e149d3204f5b5bfb93a85c685f1c05a4f4
Manvendra Singh Rawat <manvendrasingh.r11@gmail.com>
2026-05-15 09:39:17 +0530

    Technologies section

--- src/components/TechPage/tech.tsx
+++ src/components/TechPage/tech.tsx
@@ -1,11 +1,84 @@
 import "./tech.css";
 
+import csharpLogo from "../../assets/Csharp.svg";
+import cppLogo from "../../assets/cpp.svg";
+import postgresLogo from "../../assets/PostgresSQL.svg";
+import javaScriptLogo from "../../assets/JavaScript.svg";
+import dotNetLogo from "../../assets/NETCore.svg";
+import reactLogo from "../../assets/React.svg";
+import gitHubLogo from "../../assets/GitHub.svg";
+import gitLabLogo from "../../assets/GitLab.svg";
+import postmanLogo from "../../assets/Postman.svg";
+
 export default function AboutPage() {
   return (
     <>
-      <section className="section">
-        <h1>Tech Used</h1>
-      </section>
+      <div className="tech-section">
+        <div className="tech-heading"><h1>Technologies I have experience & work with</h1></div>
+
+        <div className="tech-subheading">
+          <div className="tech-block-heading">- Languages -</div>
+
+          <div className="tech-holder">
+
+            <div className="tech-used">
+              <img src={csharpLogo}></img>
+              C#
+            </div>
+            <div className="tech-used">
+              <img src={cppLogo}></img>
+              C++
+            </div>
+            <div className="tech-used">
+              <img src={postgresLogo}></img>
+              PostgreSQL
+            </div>
+            <div className="tech-used">
+              <img src={javaScriptLogo}></img>
+              JavaScript
+            </div>
+            
+          </div>
+        </div>
+
+          <div className="tech-subheading">
+            <div className="tech-block-heading">- Framework -</div>
+
+            <div className="tech-holder">
+
+              <div className="tech-used">
+                <img src={dotNetLogo}></img>
+                .NET Core
+              </div>
+              <div className="tech-used">
+                <img src={reactLogo}></img>
+                React
+              </div>
+              
+            </div>
+          </div>
+
+          <div className="tech-subheading">
+            <div className="tech-block-heading">- Tools -</div>
+
+            <div className="tech-holder">
+
+              <div className="tech-used">
+                <img src={gitHubLogo}></img>
+                Github
+              </div>
+              <div className="tech-used">
+                <img src={gitLabLogo}></img>
+                GitLab
+              </div>
+              <div className="tech-used">
+                <img src={postmanLogo}></img>
+                Postman
+              </div>
+              
+            </div>
+          </div>
+        </div>
     </>
   );
 }
